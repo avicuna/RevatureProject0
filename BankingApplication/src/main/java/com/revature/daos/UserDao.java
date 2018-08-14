6110e327c2d6ed6b350118d246aeeedaa8c0f24e
alexvicuna <vicunaalejandro@gmail.com>
2018-08-13 23:50:29 -0400

    Added user tests

--- BankingApplication/src/main/java/com/revature/daos/UserDao.java
+++ BankingApplication/src/main/java/com/revature/daos/UserDao.java
@@ -10,15 +10,16 @@ public static final UserDao currentUserDao = UserSerializer.us;
 	 * Takes in a user object and will persist that user
 	 * 
 	 * @param u
+	 * @return 
 	 */
-	void createUser(User u);
+	boolean createUser(User u);
 	User findByUsernameAndPassword(String username, String password);
 	void depositYeet(double yeet);
 	void withdrawYeet(double yeet);
-	void addHistory(String newHistory);
+	boolean addHistory(String newHistory);
 	String getHistory();
 	double getBalance();
-	void updateUser(User u);
+	boolean updateUser(User u);
 	void deleteUser(User u);
 
 }
